c723c23c9ba1c8ed722b1716b7a408c28e976048
azagatti <andre.zagatti1@gmail.com>
2020-10-13 16:18:19 -0300

    feat: create web page

---
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { Map, TileLayer } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+
+import mapMarkerImg from "../../images/map-marker.svg";
+
+import { Container } from "./styles";
+
+const OrphanagesMap: React.FC = () => {
+  return (
+    <Container>
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Pederneiras</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-22.346845, -48.7693609]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
+
+      <Link to="">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </Container>
+  );
+};
+
+export default OrphanagesMap;
