3ca01ddd2701346b162b2331e1dfea7d312a5e57
azagatti <andre.zagatti1@gmail.com>
2020-10-15 00:48:17 -0300

    feat: finished backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import "./database/connection";
 import express from "express";
 import "express-async-errors";
 import path from "path";
+import cors from "cors";
 
 import routes from "./routes";
 import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
